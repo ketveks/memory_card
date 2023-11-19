77e1a2d80bba9de5593735d53e44fef681622105
ketveks <150514488+ketveks@users.noreply.github.com>
2023-11-19 13:25:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,66 @@
 from PyQt5.QtWidgets import QApplication
-
+from random import choice,shuffle
 app = QApplication([])
+
 from main_window import *
 
 
+class Question:
+    def __init__(self,q_tex,ans,wrong1,wrong2,wrong3) :
+        self.question_tex = q_tex
+        self.answer = ans
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+    ...
+q1 = Question("Дім",  "house","hose","horse","mouse")
+q2 = Question("Миша",  "mouse","mouth","maise","mikie")
+q3 = Question("Яблуко",  "apple ","apply","ape","application")
+q4 = Question("Хмара",  "cloud","clode","chaos","chome")
 
 
+radio_list = [rb_answer_1, rb_answer_2, rb_answer_3, rb_answer_4 ]
+questionas_list = [q1,q2,q3,q4]
+
+def next_question():
+    current_question = choice(questionas_list)
+    question.setText(current_question.question_tex)
+    shuffle(radio_list)
+    radio_list[0].setText(current_question.answer)
+    radio_list[1].setText(current_question.wrong1)
+    radio_list[2].setText(current_question.wrong2)
+    radio_list[3].setText(current_question.wrong3)
+
+    correct_answer.setText(current_question.answer)
+
+next_question()
+
+def check_result():
+    RadioGroup.setExclusive(False)
+    for b in radio_list:
+        if b.isChecked():
+            if b.text() == correct_answer.text():
+                result_text.setText("Правильно!")
+            else:
+                result_text.setText("Ниправельно!")
+            b.setChecked(False)
+    RadioGroup.setExclusive(True)
+def switch_box():
+    if btn_next.text() == "Відповісти":
+        check_result()
+        RadioGroupBox.hide()
+        AnswerGroupBox.show()
+        btn_next.setText("Наступне питання")
+
+    elif btn_next.text() == "Наступне питання":
+        RadioGroupBox.show()
+        AnswerGroupBox.hide()
+        btn_next.setText("Відповісти")
+        next_question()
+btn_next.clicked.connect(switch_box)
+
+
+
+window.show()
 app.exec()
--- main_window.py
+++ main_window.py
@@ -1,69 +1,71 @@
-from PyQt5.QtWidgets import (
-    QWidget,QHBoxLayout,QVBoxLayout,
-    QLabel,QPushButton,QRadioButton,
-    QButtonGroup,QGroupBox
-)
+from PyQt5.QtWidgets import (QWidget,QHBoxLayout,QVBoxLayout,
+                            QLabel, QPushButton, QRadioButton,
+                            QGroupBox, QButtonGroup)
+
 
 window = QWidget()
 
-win_width,win_heigh = 700, 400
-window.resize(win_width, win_heigh)
-window.setWindowTitle('Memory Card')
-question = QLabel()
+win_width, wind_height = 700,400
+window.resize(win_width, wind_height)
+window.setWindowTitle("Memory Card. Автор: ....")
+
+
 btn_menu = QPushButton("Меню")
 btn_next = QPushButton("Відповісти")
 
+question = QLabel()
 
 rb_answer_1 = QRadioButton()
 rb_answer_2 = QRadioButton()
 rb_answer_3 = QRadioButton()
 rb_answer_4 = QRadioButton()
 
+RadioGroupBox = QGroupBox("Варіанти відповіді")
+
+RadioGroup = QButtonGroup()
+
+RadioGroup.addButton(rb_answer_1)
+RadioGroup.addButton(rb_answer_2)
+RadioGroup.addButton(rb_answer_3)
+RadioGroup.addButton(rb_answer_4)
 
 
-
-RadionGroup = QButtonGroup()
-
-RadionGroup.addButton(rb_answer_1)
-RadionGroup.addButton(rb_answer_2)
-RadionGroup.addButton(rb_answer_3)
-RadionGroup.addButton(rb_answer_4)
+box_h_line = QHBoxLayout()
+box_v_line_1 = QVBoxLayout()
+box_v_line_2 = QVBoxLayout()
 
 
-RadionGroupBox = QGroupBox("Варіанти відповідей")
+box_v_line_1.addWidget(rb_answer_1)
+box_v_line_1.addWidget(rb_answer_2)
 
-box_h_line =QHBoxLayout()
-box_v_line1 =QVBoxLayout()
-box_v_line2 =QVBoxLayout()
+box_v_line_2.addWidget(rb_answer_3)
+box_v_line_2.addWidget(rb_answer_4)
 
+box_h_line.addLayout(box_v_line_1)
+box_h_line.addLayout(box_v_line_2) 
 
-box_v_line1.addWidget(rb_answer_1)
-box_v_line1.addWidget(rb_answer_2)
-box_v_line2.addWidget(rb_answer_3)
-box_v_line2.addWidget(rb_answer_4)
+RadioGroupBox.setLayout(box_h_line)
 
+AnswerGroupBox = QGroupBox("Результат")
+result_text = QLabel("правильно/неправильно")
+correct_answer = QLabel("правильний варіант")
 
-box_h_line.addLayout(box_v_line1)
-box_h_line.addLayout(box_v_line2)
+answer_box_line = QVBoxLayout() 
 
+answer_box_line.addWidget(result_text)
+answer_box_line.addWidget(correct_answer)
 
+AnswerGroupBox.setLayout(answer_box_line)
 
-
-RadionGroupBox.setLayout(box_h_line)
 
 
 main_v_line = QVBoxLayout()
 
 main_v_line.addWidget(question)
-main_v_line.addWidget(RadionGroupBox)
+main_v_line.addWidget(RadioGroupBox)
+main_v_line.addWidget(AnswerGroupBox)
+AnswerGroupBox.hide()
 main_v_line.addWidget(btn_next)
 
 
-
 window.setLayout(main_v_line)
-
-
-
-
-
-window.show()
